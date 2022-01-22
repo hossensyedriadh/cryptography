3fa5922ace7c2c8559757b1830031a220ab06156
hossensyedriadh <48594108+hossensyedriadh@users.noreply.github.com>
2022-01-23 03:31:09 +0600

    view and style changes, menu added

--- src/main/resources/static/js/asymmetric-decryption.js
+++ src/main/resources/static/js/asymmetric-decryption.js
@@ -1,10 +1,28 @@
 let decryption_form = document.getElementById('decryption_form');
+let encrypted_text_field = document.getElementById('encrypted_text_field');
+let encrypted_text = document.getElementById('encrypted_text');
+let input_error = document.getElementById('input_error');
 let private_key = document.getElementById('private_key');
 let private_key_field = document.getElementById('private_key_field');
 let private_key_error = document.getElementById('private_key_error');
 let submit_button = document.getElementById('submit_button');
 
+input_error.style.display = 'none';
 private_key_error.style.display = 'none';
+
+function validateInput() {
+    const key_regex = /^([A-Za-z0-9+\/]{4})*([A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{2}==)?$/;
+
+    if (key_regex.test(encrypted_text.value)) {
+        submit_button.disabled = false;
+        input_error.style.display = 'none';
+        encrypted_text_field.className = 'field';
+    } else {
+        submit_button.disabled = true;
+        input_error.style.display = 'block';
+        encrypted_text_field.className = 'field error';
+    }
+}
 
 function validatePrivateKey() {
     if (private_key.value.startsWith("-----BEGIN RSA PRIVATE KEY-----\n")
